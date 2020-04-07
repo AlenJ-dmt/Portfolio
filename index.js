7a99224cb29ba86e8966a689f3b4bd59793e03d7
Alen Diaz <noricual@gmail.com>
2020-04-07 11:42:11 -0500

    navigation_done

---
+++ index.js
@@ -0,0 +1,22 @@
+function toggle_hamburger_menu(){
+    $('#menu_logo').click(  ()  => {
+        if( $('#menu_list').height() === 0){
+            console.log('ping')
+            $('#menu_list').css('height', 300)
+        }
+        else if( $('#menu_list').height() !== 0){
+            $('#menu_list').css('height', 0)
+        }
+    })
+}
+function close_menu_on_click(){
+    $('#menu_list div a').click( () =>{
+        console.log('ping')
+        $('#menu_list').height(0);
+    })
+}
+function main(){
+    toggle_hamburger_menu();
+    close_menu_on_click();
+}
+main();
