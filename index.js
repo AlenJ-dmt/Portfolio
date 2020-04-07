ba0279ea63b5719a1570fa4fa5484cf882354410
Alen Diaz <noricual@gmail.com>
2020-04-07 12:08:39 -0500

    navBar_corrected

--- index.js
+++ index.js
@@ -15,8 +15,16 @@ function close_menu_on_click(){
         $('#menu_list').height(0);
     })
 }
+function close_menu_on_screen_touch(){
+    $('body').click( () => {
+        if( $('#menu_list').height() !== 0){
+            $('#menu_list').css('height', 0)
+        }
+    })
+}
 function main(){
     toggle_hamburger_menu();
     close_menu_on_click();
+    close_on_screen_touch();
 }
 main();
